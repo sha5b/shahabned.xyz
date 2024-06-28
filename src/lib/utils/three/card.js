b8694c1429e917a9436468da0ea1445390a96a31
sha5b <ned.tabulov@gmail.com>
2024-06-28 09:58:34 +0200

    Refactor
    ALso Preloading Font Files before the site Loads

--- src/lib/utils/three/card.js
+++ src/lib/utils/three/card.js
@@ -1,186 +1,90 @@
-//src/lib/utils/three/card.js
+// src/lib/utils/three/card.js
 import * as THREE from 'three';
 import { getImageURL } from '$lib/utils/getURL';
 import { goto } from '$app/navigation';
+import { createIconTexture, createTextTexture } from '$lib/utils/three/text';
 
 function createRoundedRectTexture(width, height, radius, resolution = 1024, color = '#f5f5f5') {
-	const canvas = document.createElement('canvas');
-	canvas.width = resolution;
-	canvas.height = resolution * (height / width);
-	const context = canvas.getContext('2d');
-	const scale = resolution / width;
-
-	context.scale(scale, scale);
-
-	context.fillStyle = color;
-	context.beginPath();
-	context.moveTo(radius, 0);
-	context.lineTo(width - radius, 0);
-	context.quadraticCurveTo(width, 0, width, radius);
-	context.lineTo(width, height - radius);
-	context.quadraticCurveTo(width, height, width - radius, height);
-	context.lineTo(radius, height);
-	context.quadraticCurveTo(0, height, 0, height - radius);
-	context.lineTo(0, radius);
-	context.quadraticCurveTo(0, 0, radius, 0);
-	context.closePath();
-	context.fill();
-
-	return new THREE.CanvasTexture(canvas);
-}
-
-function createIconTexture(icon, color, width = 640, height = 1024) {
-	const canvas = document.createElement('canvas');
-	canvas.width = width;
-	canvas.height = height;
-	const context = canvas.getContext('2d');
-
-	context.clearRect(0, 0, width, height);
-
-	const radius = Math.min(width, height) / 6;
-	context.fillStyle = 'black';
-	context.beginPath();
-	context.arc(width / 2, height / 2, radius, 0, Math.PI * 2);
-	context.fill();
-
-	context.fillStyle = color;
-	context.font = `${radius}px 'Material Icons'`;
-	context.textAlign = 'center';
-	context.textBaseline = 'middle';
-	context.fillText(icon, width / 2, height / 2);
-
-	return new THREE.CanvasTexture(canvas);
-}
-function createTextTexture(title, date, type, format, width, height, fontSize = 18, color = 'black', pageType = 'category') {
     const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = resolution;
+    canvas.height = resolution * (height / width);
     const context = canvas.getContext('2d');
+    const scale = resolution / width;
 
-    context.clearRect(0, 0, width, height);
+    context.scale(scale, scale);
+
     context.fillStyle = color;
-    context.font = `${fontSize}px Oxanium`;
-    context.textAlign = 'left';
-    context.textBaseline = 'bottom';
-
-    // Function to draw text with specified alignment and position
-    const drawText = (text, x, y, align = 'left', baseline = 'bottom', bold = false) => {
-        if (typeof text !== 'string') return;
-
-        context.textAlign = align;
-        context.textBaseline = baseline;
-        context.font = bold ? `bold ${fontSize}px Oxanium` : `${fontSize}px Oxanium`;
-
-        // Calculate the maximum width for the text
-        const maxWidth = width - 20; // 10 pixels padding from both edges
-
-        // Split the text into lines
-        const words = text.split(' ');
-        let line = '';
-        const lines = [];
-        for (let n = 0; n < words.length; n++) {
-            const testLine = line + words[n] + ' ';
-            const metrics = context.measureText(testLine);
-            const testWidth = metrics.width;
-            if (testWidth > maxWidth && n > 0) {
-                lines.push(line);
-                line = words[n] + ' ';
-            } else {
-                line = testLine;
-            }
-        }
-        lines.push(line);
-
-        // Draw each line of text
-        for (let i = 0; i < lines.length; i++) {
-            context.fillText(lines[i], x, y);
-            y += fontSize;
-        }
-    };
-
-    if (pageType !== 'landing') {
-        // Format date
-        const formattedDate = date ? new Intl.DateTimeFormat('en-US', {
-            year: 'numeric', month: 'short'
-        }).format(new Date(date)) : '';
-
-        // Draw date at top left and type at top right
-        if (formattedDate || type) {
-            drawText(formattedDate, 10, 10 + fontSize, 'left', 'top');
-            drawText(type, width - 10, 10 + fontSize, 'right', 'top');
-        }
-
-        // Draw format above title with some padding
-        const paddingBetweenFormatAndTitle = 20;
-        if (format) {
-            drawText(format, 10, height - 40 - paddingBetweenFormatAndTitle - fontSize, 'left', 'bottom');
-        }
-    }
-
-    // Draw title at bottom left in bold
-    if (title) {
-        drawText(title, 10, height - 40, 'left', 'bottom', true);
-    }
+    context.beginPath();
+    context.moveTo(radius, 0);
+    context.lineTo(width - radius, 0);
+    context.quadraticCurveTo(width, 0, width, radius);
+    context.lineTo(width, height - radius);
+    context.quadraticCurveTo(width, height, width - radius, height);
+    context.lineTo(radius, height);
+    context.quadraticCurveTo(0, height, 0, height - radius);
+    context.lineTo(0, radius);
+    context.quadraticCurveTo(0, 0, radius, 0);
+    context.closePath();
+    context.fill();
 
     return new THREE.CanvasTexture(canvas);
 }
 
-
 function createMaterial(textureURL, itemWidth, itemHeight, radius = 8) {
-	const roundedRectTexture = createRoundedRectTexture(itemWidth * 100, itemHeight * 100, radius);
-	let material;
+    const roundedRectTexture = createRoundedRectTexture(itemWidth * 100, itemHeight * 100, radius);
+    let material;
 
-	if (textureURL) {
-		const loader = new THREE.TextureLoader();
-		const texture = loader.load(textureURL, (texture) => {
-			texture.colorSpace = THREE.SRGBColorSpace;
+    if (textureURL) {
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load(textureURL, (texture) => {
+            texture.colorSpace = THREE.SRGBColorSpace;
 
-			const aspectRatio = texture.image.width / texture.image.height;
-			const cardAspectRatio = itemWidth / itemHeight;
+            const aspectRatio = texture.image.width / texture.image.height;
+            const cardAspectRatio = itemWidth / itemHeight;
 
-			if (aspectRatio > cardAspectRatio) {
-				texture.repeat.set(cardAspectRatio / aspectRatio, 1);
-				texture.offset.set((1 - texture.repeat.x) / 2, 0);
-			} else {
-				texture.repeat.set(1, aspectRatio / cardAspectRatio);
-				texture.offset.set(0, (1 - texture.repeat.y) / 2);
-			}
+            if (aspectRatio > cardAspectRatio) {
+                texture.repeat.set(cardAspectRatio / aspectRatio, 1);
+                texture.offset.set((1 - texture.repeat.x) / 2, 0);
+            } else {
+                texture.repeat.set(1, aspectRatio / cardAspectRatio);
+                texture.offset.set(0, (1 - texture.repeat.y) / 2);
+            }
 
-			texture.wrapS = THREE.ClampToEdgeWrapping;
-			texture.wrapT = THREE.ClampToEdgeWrapping;
-			material.needsUpdate = true;
-		});
+            texture.wrapS = THREE.ClampToEdgeWrapping;
+            texture.wrapT = THREE.ClampToEdgeWrapping;
+            material.needsUpdate = true;
+        });
 
-		material = new THREE.MeshPhongMaterial({
-			map: texture,
-			alphaMap: roundedRectTexture,
-			transparent: true,
-			color: 0xffffff,
-			shininess: 30
-		});
+        material = new THREE.MeshPhongMaterial({
+            map: texture,
+            alphaMap: roundedRectTexture,
+            transparent: true,
+            color: 0xffffff,
+            shininess: 30
+        });
 
-		texture.onUpdate = function () {
-			this.needsUpdate = false;
-		};
-	} else {
-		material = new THREE.MeshPhongMaterial({
-			map: roundedRectTexture,
-			transparent: true,
-			color: 0xffffff,
-			shininess: 30
-		});
-	}
+        texture.onUpdate = function () {
+            this.needsUpdate = false;
+        };
+    } else {
+        material = new THREE.MeshPhongMaterial({
+            map: roundedRectTexture,
+            transparent: true,
+            color: 0xffffff,
+            shininess: 30
+        });
+    }
 
-	return material;
+    return material;
 }
 
 function parseHexColor(hex) {
-	try {
-		return new THREE.Color(hex);
-	} catch {
-		console.error(`Invalid color: ${hex}`);
-		return new THREE.Color(0xffffff);
-	}
+    try {
+        return new THREE.Color(hex);
+    } catch {
+        console.error(`Invalid color: ${hex}`);
+        return new THREE.Color(0xffffff);
+    }
 }
 
 function createCardMesh(itemWidth, itemHeight, textureURL, radius = 8, onClick = null, cardColor = null, mainText = null, mainTextColor = 'black', date = '', type = '', format = '', pageType = 'category') {
@@ -219,43 +123,42 @@ function createCardMesh(itemWidth, itemHeight, textureURL, radius = 8, onClick =
     return cardMesh;
 }
 
-
 function createNavigationCardMesh(itemWidth, itemHeight, icon, color, onClick) {
-	const radius = 8;
-	const roundedRectTexture = createRoundedRectTexture(itemWidth * 100, itemHeight * 100, radius);
-	const parsedColor = parseHexColor(color); // Ensure this function works correctly
-	const cardMaterial = new THREE.MeshPhongMaterial({
-		map: roundedRectTexture,
-		transparent: true,
-		color: parsedColor,
-		shininess: 30
-	});
+    const radius = 8;
+    const roundedRectTexture = createRoundedRectTexture(itemWidth * 100, itemHeight * 100, radius);
+    const parsedColor = parseHexColor(color); // Ensure this function works correctly
+    const cardMaterial = new THREE.MeshPhongMaterial({
+        map: roundedRectTexture,
+        transparent: true,
+        color: parsedColor,
+        shininess: 30
+    });
 
-	const cardMesh = new THREE.Mesh(new THREE.PlaneGeometry(itemWidth, itemHeight), cardMaterial);
-	cardMesh.receiveShadow = true;
+    const cardMesh = new THREE.Mesh(new THREE.PlaneGeometry(itemWidth, itemHeight), cardMaterial);
+    cardMesh.receiveShadow = true;
 
-	if (onClick) {
-		cardMesh.userData = { onClick };
-		// @ts-ignore
-		cardMesh.callback = onClick;
-	}
+    if (onClick) {
+        cardMesh.userData = { onClick };
+        // @ts-ignore
+        cardMesh.callback = onClick;
+    }
 
-	const iconTexture = createIconTexture(icon, color);
-	const iconMaterial = new THREE.MeshBasicMaterial({ map: iconTexture, transparent: true });
-	const iconMesh = new THREE.Mesh(new THREE.PlaneGeometry(itemWidth, itemHeight), iconMaterial);
+    const iconTexture = createIconTexture(icon, color);
+    const iconMaterial = new THREE.MeshBasicMaterial({ map: iconTexture, transparent: true });
+    const iconMesh = new THREE.Mesh(new THREE.PlaneGeometry(itemWidth, itemHeight), iconMaterial);
 
-	iconMesh.position.set(0, 0, 0.25);
-	iconMesh.castShadow = true;
-	iconMesh.raycast = () => {};
+    iconMesh.position.set(0, 0, 0.25);
+    iconMesh.castShadow = true;
+    iconMesh.raycast = () => {};
 
-	cardMesh.add(iconMesh);
+    cardMesh.add(iconMesh);
 
-	return cardMesh;
+    return cardMesh;
 }
 
 function addCard(gridContainer, cardMesh, x, y) {
-	cardMesh.position.set(x, y, 0);
-	gridContainer.add(cardMesh);
+    cardMesh.position.set(x, y, 0);
+    gridContainer.add(cardMesh);
 }
 
 function addWorkCard(gridContainer, work, x, y, itemWidth, itemHeight, onClick, pageType) {
@@ -290,7 +193,6 @@ function addWorkCard(gridContainer, work, x, y, itemWidth, itemHeight, onClick, 
     addCard(gridContainer, cardMesh, x, y);
 }
 
-
 function addCategoryCard(gridContainer, category, x, y, itemWidth, itemHeight, onClick) {
     const textureURL = getImageURL('category', category.id, category.thump, '400x600');
     const cardMesh = createCardMesh(itemWidth, itemHeight, textureURL, 8, () => {
@@ -300,32 +202,31 @@ function addCategoryCard(gridContainer, category, x, y, itemWidth, itemHeight, o
             goto(`/${category.title}`);
         }
     }, null, category.title); // Pass category title as text
-	
+    
     addCard(gridContainer, cardMesh, x, y);
 }
 
 function addNavigationCard(gridContainer, icon, color, x, y, itemWidth, itemHeight, onClick) {
-	const cardMesh = createNavigationCardMesh(itemWidth, itemHeight, icon, color, onClick);
-	addCard(gridContainer, cardMesh, x, y);
+    const cardMesh = createNavigationCardMesh(itemWidth, itemHeight, icon, color, onClick);
+    addCard(gridContainer, cardMesh, x, y);
 }
 
 function addImageCard(gridContainer, image, x, y, itemWidth, itemHeight) {
-	const textureURL = getImageURL('works', image.id, image.thump, '400x600');
-	const cardMesh = createCardMesh(itemWidth, itemHeight, textureURL, 8, () => {
-		console.log('Image card clicked:', image);
-	});
+    const textureURL = getImageURL('works', image.id, image.thump, '400x600');
+    const cardMesh = createCardMesh(itemWidth, itemHeight, textureURL, 8, () => {
+        console.log('Image card clicked:', image);
+    });
 
-	addCard(gridContainer, cardMesh, x, y);
+    addCard(gridContainer, cardMesh, x, y);
 }
 
 export {
-	createRoundedRectTexture,
-	createIconTexture,
-	createMaterial,
-	createCardMesh,
-	addCard,
-	addWorkCard,
-	addCategoryCard,
-	addNavigationCard,
-	addImageCard
+    createRoundedRectTexture,
+    createMaterial,
+    createCardMesh,
+    addCard,
+    addWorkCard,
+    addCategoryCard,
+    addNavigationCard,
+    addImageCard
 };
