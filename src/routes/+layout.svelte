f44d444b0a1ae68b1a971e2e01abf8667878b48b
sha5b <ned.tabulov@gmail.com>
2024-07-12 19:13:58 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,12 @@
+<svelte:head>
+  <title>Shahab Nedaei - Experimental Digital Art</title>
+  <meta name="description" content="Shahab Nedaei's experimental art practice delves into the unconscious aspects of identity through digital, virtual reality, and AI art.">
+  <meta name="keywords" content="digital art, virtual reality art, AI art, experimental art, cultural identity art, Shahab Nedaei">
+  <meta property="og:title" content="Shahab Nedaei - Experimental Digital and AI Art">
+  <meta property="og:description" content="Exploring the unconscious aspects of identity through digital and AI art.">
+  <meta name="twitter:card" content="summary_large_image">
+</svelte:head>
+
 <main>
 	<slot />
 </main>
